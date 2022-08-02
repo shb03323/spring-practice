8e1380e1666dabe19997938c1ee195cb774988b2
jeonghoon <shb03323@naver.com>
2022-08-02 12:42:14 +0900

    [Init] initialization

---
+++ spring_practice/src/test/java/practice/spring_practice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.spring_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
