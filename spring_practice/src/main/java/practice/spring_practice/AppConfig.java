35b61b69b7e8ba51f230bc15a1141acf8d8f7fdd
jeonghoon <shb03323@naver.com>
2022-08-04 01:22:03 +0900

    [Fix] Appconfig 의존성 분리

--- spring_practice/src/main/java/practice/spring_practice/AppConfig.java
+++ spring_practice/src/main/java/practice/spring_practice/AppConfig.java
@@ -1,6 +1,8 @@
 package practice.spring_practice;
 
+import practice.spring_practice.discount.DiscountPolicy;
 import practice.spring_practice.discount.FixDiscountPolicy;
+import practice.spring_practice.member.MemberRepository;
 import practice.spring_practice.member.MemberService;
 import practice.spring_practice.member.MemberServiceImpl;
 import practice.spring_practice.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import practice.spring_practice.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
