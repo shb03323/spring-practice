12419a125ed4613a3fccd7e6e1aa5a2612a2327a
jeonghoon <shb03323@naver.com>
2022-08-03 14:19:04 +0900

    [Feat] add MemberApp to check storing

---
+++ spring_practice/src/main/java/practice/spring_practice/MemberApp.java
@@ -0,0 +1,18 @@
+package practice.spring_practice;
+
+import practice.spring_practice.member.Grade;
+import practice.spring_practice.member.Member;
+import practice.spring_practice.member.MemberService;
+import practice.spring_practice.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
