8a8f48c2f66963506c172367d7a5ee43f8fa7f4c
jeonghoon <shb03323@naver.com>
2022-08-03 18:55:27 +0900

    [Feat] add RateDiscountPolicy Case

---
+++ spring_practice/src/main/java/practice/spring_practice/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package practice.spring_practice.discount;
+
+import practice.spring_practice.member.Grade;
+import practice.spring_practice.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
