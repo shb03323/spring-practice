f6c84a444f27cb1cc419b99b6ab2157beb906236
jeonghoon <shb03323@naver.com>
2022-08-10 00:28:16 +0900

    [Feat] setter 만들어서 의존관계 주입

--- spring_practice/src/main/java/practice/spring_practice/order/OrderServiceImpl.java
+++ spring_practice/src/main/java/practice/spring_practice/order/OrderServiceImpl.java
@@ -12,8 +12,20 @@ import practice.spring_practice.member.MemoryMemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("memberRepository = " + memberRepository);
