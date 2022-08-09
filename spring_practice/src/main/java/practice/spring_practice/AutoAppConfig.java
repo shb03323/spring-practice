308e7fd9b091befd26f0e47f4cbe4af2786c9c60
jeonghoon <shb03323@naver.com>
2022-08-09 21:01:44 +0900

    [Feat] testing bean

--- spring_practice/src/main/java/practice/spring_practice/AutoAppConfig.java
+++ spring_practice/src/main/java/practice/spring_practice/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package practice.spring_practice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import practice.spring_practice.member.MemberRepository;
+import practice.spring_practice.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -11,4 +14,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 }
