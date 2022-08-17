4ad46c5a95133489db2cc326b4bd711f890d01a1
jeonghoon <shb03323@naver.com>
2022-08-17 22:44:02 +0900

    [Feat] test by logger

--- spring_practice/src/main/java/practice/spring_practice/web/LogDemoService.java
+++ spring_practice/src/main/java/practice/spring_practice/web/LogDemoService.java
@@ -9,10 +9,9 @@ import practice.spring_practice.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
